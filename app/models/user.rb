9f41acd0dad7a468d5a6d287394a1429e0a75540
Manny <manny.wetterqvist@outlook.com>
2023-11-27 21:56:57 +0900

    Basic user setup and models

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: 'User'
+end
