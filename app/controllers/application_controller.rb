98e161b8444b61e1bbeccdc4f65bc105e902feb3
Manny W <manny.wetterqvist@outlook.com>
2024-01-23 10:02:57 +0900

    Working on login flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  def authenticate_user!
+    if user_signed_in?
+
+      super
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 end
