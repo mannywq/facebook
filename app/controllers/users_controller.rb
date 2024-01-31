c551cf4a2e3eed0073908d2a893994da20459b5f
Manny <manny.wetterqvist@outlook.com>
2024-01-31 18:48:44 +0900

    Layout updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.where.not(email: current_user.email).page(1).per(15).order(created_at: :desc)
+    @users = User.all.excluding(current_user)
   end
 
   def show
