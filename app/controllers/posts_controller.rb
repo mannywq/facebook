f9ed51562367c4039aa2759311e614fa8d9dfbef
Manny <manny.wetterqvist@outlook.com>
2024-02-01 18:53:32 +0900

    Working on posts modal feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @friends = current_user.friends.pluck(:id)
 
     @posts = Post.joins(:user).where(users: { id: @friends }).order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+
+    @post.save!
+
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_033201) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_01_032400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_033201) do
     t.index ["likeable_type", "likeable_id", "user_id"], name: "index_likes_on_likeable_type_and_likeable_id_and_user_id", unique: true
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
