3261fcd0626202039bdee66b7db309e80ea8a96e
Manny W <manny.wetterqvist@outlook.com>
2024-02-01 10:27:59 +0900

    Added friends post feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+  def index
+    @friends = current_user.friends.pluck(:id)
+
+    @posts = Post.joins(:user).where(users: { id: @friends }).order(created_at: :desc)
+  end
 end
