d1682ae8f806cbcf3f770cc8b0cc66de579e461b
Manny W <manny.wetterqvist@outlook.com>
2024-02-13 12:55:59 +0900

    updated button classes and small tweaks to app template

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,9 @@
 module FormHelper
   def primary_btn_classes
-    'p-2 rounded-lg font-medium text-lg text-white bg-blue-700'
+    'p-2 rounded-lg font-semibold text-sm text-white bg-blue-700'
   end
 
   def secondary_btn_classes
-    'p-2 rounded-lg font-bold text-sm text-gray-600 bg-gray-200'
+    'p-2 rounded-lg font-semibold text-sm text-gray-600 bg-gray-100'
   end
 end
