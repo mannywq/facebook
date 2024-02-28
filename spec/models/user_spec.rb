a63a91e7062393929da7d5124350ec9235abab8c
Manny W <manny.wetterqvist@outlook.com>
2024-02-28 20:16:29 +0900

    Working on model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  let(:user) { create(:user) }
+  before(:each) do
+    @user = User.new
+  end
 
   it 'Accepts a valid user' do
     expect(user).to be_valid
+  end
+
+  it 'doesn\'t accept a user without a name' do
+    @user.email = 'test@test.com'
+
+    expect(@user.save).to be_falsey
   end
 end
